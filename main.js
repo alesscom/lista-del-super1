c7b7c097242c765276c8c7a1d50c1386e408e0d5
alesscom <90730443+alesscom@users.noreply.github.com>
2023-09-19 18:40:01 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsF6ZKlbh2d4gXwlXpWA05Jwjxk-Zh5iI",
+    authDomain: "lista-de-compras-b8b4c.firebaseapp.com",
+    projectId: "lista-de-compras-b8b4c",
+    storageBucket: "lista-de-compras-b8b4c.appspot.com",
+    messagingSenderId: "646219767875",
+    appId: "1:646219767875:web:9fc1d0d0b24080f22774f6"
+  };
+  
+  // Initialize Firebase
+  var db = firebase.firestore();
